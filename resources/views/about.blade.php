8215a46d20fc9cedca6395cb970b24ca269a62cc
Ibtissam <btissam1234btissam12@gmail.com>
2023-03-07 12:55:02 +0100

    secund changes

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+@section('content')
+
+    <div class="flex justify-center mt-10">
+       <h1 class="text"> ABOUT US</h1>
+    </div>
+<div class=" text-center items-center justify-center rounded-full mt-10">
+                        
+                         welcome to our About page           
+</div>
+
+@endsection
+@section('title','About')
