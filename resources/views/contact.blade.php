4e1be14011b467fefacb1aa64e540ac8cd6cdc9d
Ibtissam <btissam1234btissam12@gmail.com>
2023-03-07 12:55:54 +0100

    secund changes

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+@section('content')
+
+     <div class="flex justify-center">
+       <h1 class="text"> CONTACT US</h1>  
+     </div>
+ <div class=" text-center items-center justify-center rounded-full">
+                         
+                          welcome to our contact  page           
+ </div>
+    </body>
+@endsection
+@section('title','Contact')
