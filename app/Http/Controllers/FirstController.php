8421dc5b96adc130ffd0b8ef08db052380451d28
Ibtissam <btissam1234btissam12@gmail.com>
2023-03-25 16:08:55 +0100

    this the version of static controllers i do a mistake

--- app/Http/Controllers/FirstController.php
+++ app/Http/Controllers/FirstController.php
@@ -11,5 +11,7 @@ class FirstController extends Controller
    public function contact(){return view('contact') ;}
    public function portfolio(){return '<h1> you are in the portfolio  page</h1>';}
 }
+// In controllers classe on définie les méthodes et les views que on créé avec le type de retpur et cela using des fonction pour chaque view
 //here we define classe of controller with its methode 
-//we creat it using command line php artisan make:controller <name of controller > we can also write php artisan -h for show all the command we can do 
+//we creat it using command line php artisan make:controller <name of controller > we can also write php artisan -h for show all the command we can do
+//This controllor show all views normal so each view with an controller for play it
