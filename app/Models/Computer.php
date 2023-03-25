ef9fbba71d5331ade91be9a2c2aca640a7daedb9
Ibtissam <btissam1234btissam12@gmail.com>
2023-03-25 16:15:28 +0100

    database model

---
+++ app/Models/Computer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Computer extends Model
+{
+    use HasFactory;
+}
+// we create this model for using data base and class and for create it we use php artisan make:model ModelName
